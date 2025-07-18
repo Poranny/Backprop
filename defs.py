8a70e70ee564a9c087ad6d193ea6d09db73874d7
Poranny <adam@poranny.it>
2025-07-19 00:22:43 +0200

    Further definitions

--- defs.py
+++ defs.py
@@ -1,14 +1,23 @@
 class Neuron:
     def __init__(self) :
-        self.weights = []
-        self.biases = []
+        self.bias = 0.0
         self.inputs = []
 
-
+        self.activation = None
 
         self.output = None
 
+    def add_input(self, input_neuron):
+        self.inputs.append((input_neuron, 0.0))
+
+    def add_output(self, output_neuron):
+        self.output = output_neuron
+
+    def set_activation(self, activation):
+        self.activation = activation
 
 class Layer:
-    def __init__(self) :
-        self.neurons = []
+    def __init__(self, net_size : int) :
+        self.net_size = net_size
+
+        self.neurons = [Neuron() for _ in range(self.net_size)]
