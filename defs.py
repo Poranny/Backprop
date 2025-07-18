6ab7b196cc8c02bb011e02f4f968074cd75a5c85
Poranny <adam@poranny.it>
2025-07-19 00:17:17 +0200

    Initial commit

---
+++ defs.py
@@ -0,0 +1,14 @@
+class Neuron:
+    def __init__(self) :
+        self.weights = []
+        self.biases = []
+        self.inputs = []
+
+
+
+        self.output = None
+
+
+class Layer:
+    def __init__(self) :
+        self.neurons = []
